40ed39b08a9b581feba77e1fba43108d6800d2fc
JokerMSD <igor332004@gmail.com>
2024-02-15 18:24:43 -0300

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-import express, { json } from "express";
+import "express-async-errors";
+import express, { json} from "express";
+import routes from "./Routes/routes";
+import { GlobalErrors } from "./middlewares/middleware";
+import helmet from "helmet";
+
 
 export const app = express();
-
+app.use(helmet())
 app.use(json());
+const globalErrors = new GlobalErrors();
+
+app.use(routes);
+
+app.use(globalErrors.handleErrors);
