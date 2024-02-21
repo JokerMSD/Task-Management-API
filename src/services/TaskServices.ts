1110d445b9cf23df971487cf7541a3eebd2e9336
JokerMSD <igor332004@gmail.com>
2024-02-21 17:42:13 -0300

    update TaskServices.ts

--- src/services/TaskServices.ts
+++ src/services/TaskServices.ts
@@ -1,10 +1,9 @@
-import { Request, Response } from "express";
-import { Category, Task} from "@prisma/client";
+import { Request, Response, request } from "express";
+import { Category, Task } from "@prisma/client";
 import { prisma } from "../database/database";
 import { CreateTask } from "../interfaces/interfaces";
 
 export class TaskClass implements Task {
-
   id: number;
   title: string;
   content: string;
@@ -18,7 +17,7 @@ export class TaskClass implements Task {
     content: string,
     finished: boolean,
     categoryId: number,
-    category: any[]
+    category: any[],
   ) {
     this.id = id;
     this.title = title;
@@ -27,92 +26,189 @@ export class TaskClass implements Task {
     this.categoryId = categoryId;
     this.category = category;
   }
+  public toResponseObject(): any {
+    return {
+      id: this.id,
+      title: this.title,
+      content: this.content,
+      finished: this.finished,
+      category: this.category,
+    };
+  }
 }
 
 export class TaskController {
   public idCounter: number = 1;
 
-public getTasks = async (req: Request, res: Response): Promise<Response> => {
-  try {
-    const queryParams = req.query.category ? parseInt(String(req.query.category)) : undefined;
-    
-    const categoryIdFilter = typeof queryParams === 'number' ? { equals: queryParams } : undefined;
+  public getTasks = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const categoryNameFilter = req.query.category
+        ? String(req.query.category).toLowerCase()
+        : undefined;
 
-    const matchingTasks = await prisma.task.findMany({ 
-      where: { categoryId: categoryIdFilter },
-      include: { category: true }
-    });
-    return res.status(200).json(matchingTasks);
-  } catch (error) {
-    console.error("Error fetching tasks:", error);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-}
+      let whereClause = {};
+      if (categoryNameFilter) {
+        whereClause = {
+          category: {
+            name: { equals: categoryNameFilter, mode: "insensitive" },
+          },
+        };
+      }
 
-public getTaskById = async (req: Request, res: Response): Promise<Response> => {
-  try {
-    const taskId = Number(req.params.id);
-    const task = await prisma.task.findUnique({ 
-      where: { id: taskId },
-      include: { category: true }
-    });
+      const matchingTasks = await prisma.task.findMany({
+        where: whereClause,
+        include: { category: true },
+      });
 
-    if (!task) {
-      return res.status(404).json({ error: "Task not found." });
+      if (matchingTasks.length === 0) {
+        return res.status(404).json({ message: "No tasks found" });
+      }
+
+      const response = matchingTasks.map((task) => {
+        return {
+          id: task.id,
+          title: task.title,
+          content: task.content,
+          finished: task.finished,
+          category: task.category,
+        };
+      });
+
+      return res.status(200).json(response);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+      return res.status(500).json({ error: "Internal server error" });
     }
+  };
 
-    return res.status(200).json(task);
-  } catch (error) {
-    console.error("Error fetching task:", error);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-}
+  public getTaskById = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    try {
+      const taskId = Number(req.params.id);
+      const matchingTasks = await prisma.task.findMany({
+        where: { id: taskId },
+        include: { category: true },
+      });
 
-  public createTask = async (req: Request, res: Response): Promise<Response> => {
+      if (matchingTasks.length === 0) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+
+      const response = matchingTasks.map((task) => {
+        return {
+          id: task.id,
+          title: task.title,
+          content: task.content,
+          finished: task.finished,
+          category: task.category,
+        };
+      });
+
+      if (!matchingTasks) {
+        return res.status(404).json({ error: "Task not found." });
+      }
+
+      return res.status(200).json(response);
+    } catch (error) {
+      console.error("Error fetching task:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+  public createTask = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
     try {
       const newTask: CreateTask = req.body;
+
+      if (typeof req.body.title !== "string") {
+        return res.status(400).json({ error: "Title must be a string" });
+      }
+
+      const category = await prisma.category.findUnique({
+        where: { id: newTask.categoryId },
+      });
+      if (!category) {
+        return res.status(404).json({ message: "Category not found" });
+      }
+
       const createdTask = await prisma.task.create({ data: newTask });
+
       return res.status(201).json(createdTask);
     } catch (error) {
-      console.error("Error creating task:", error);
-      return res.status(404).json({ message: "Category not found" });
+      if (error instanceof Error) {
+        console.error("Error creating task:", error);
+      } else {
+        console.error("An unexpected error occurred:", error);
+      }
+      return res
+        .status(500)
+        .json({ message: "An error occurred while creating the task" });
     }
-  }
+  };
 
-  public updateTask = async (req: Request, res: Response): Promise<Response> => {
+  public updateTask = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
     try {
       const taskId = Number(req.params.id);
       const updatedTaskData = req.body;
-      const updated = await prisma.task.update({ where: { id: taskId }, data: updatedTaskData });
 
-      if (!updated) {
-        return res.status(404).json({ error: "Task not found." });
+      const existingTask = await prisma.task.findUnique({
+        where: { id: taskId },
+      });
+      if (!existingTask) {
+        return res.status(404).json({ message: "Task not found" });
       }
+
+      if (updatedTaskData.categoryId) {
+        const existingCategory = await prisma.category.findUnique({
+          where: { id: updatedTaskData.categoryId },
+        });
+        if (!existingCategory) {
+          return res.status(404).json({ message: "Category not found" });
+        }
+      }
+
+      const updated = await prisma.task.update({
+        where: { id: taskId },
+        data: updatedTaskData,
+      });
 
       return res.status(200).json(updated);
     } catch (error) {
       console.error("Error updating task:", error);
       return res.status(500).json({ error: "Internal server error" });
     }
-  }
+  };
 
-  public deleteTask = async (req: Request, res: Response): Promise<Response> => {
+  public deleteTask = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
     try {
       const taskId = Number(req.params.id);
-      const deleted = await prisma.task.delete({ where: { id: taskId } });
 
-      if (!deleted) {
-        return res.status(404).json({ error: "Task not found." });
+      const existingTask = await prisma.task.findUnique({
+        where: { id: taskId },
+      });
+
+      if (!existingTask) {
+        return res.status(404).json({ message: "Task not found" });
       }
+
+      await prisma.task.delete({ where: { id: taskId } });
 
       return res.status(204).send();
     } catch (error) {
       console.error("Error deleting task:", error);
       return res.status(500).json({ error: "Internal server error" });
     }
-  }
+  };
 }
-
-
 
 export default TaskController;
