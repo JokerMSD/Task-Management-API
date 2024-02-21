edfdd9271c0c47244a75dee0c808e9ef0616baa4
JokerMSD <igor332004@gmail.com>
2024-02-21 17:42:09 -0300

    update routes.ts

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,7 +1,7 @@
 import { TaskController } from "../services/TaskServices";
 import { CategoryController } from "../services/CategoryServices";
 import { Router } from "express";
-import { CheckTaskExistence, CheckDuplicateTaskName, GlobalErrors } from "../middlewares/middleware";
+import { CheckDuplicateTaskName, GlobalErrors } from "../middlewares/middleware";
 import { taskCreateSchema, taskUpdateSchema, categoryCreateSchema } from "../schemas/task.schemas";
 
 const router = Router();
@@ -11,12 +11,13 @@ const categoryController = new CategoryController();
 
 
 
-router.post("/tasks", CheckDuplicateTaskName.getInstance().execute, globalErrors.validateBody(taskCreateSchema), taskController.createTask);
+router.post("/tasks", CheckDuplicateTaskName.getInstance().execute, globalErrors.validateTitle, globalErrors.validateBody(taskCreateSchema), taskController.createTask);
 router.get('/tasks', taskController.getTasks);
 router.get("/tasks/:id", taskController.getTaskById);
 router.patch("/tasks/:id",globalErrors.validateBody(taskUpdateSchema), taskController.updateTask);
 router.delete("/tasks/:id", taskController.deleteTask);
 
+router.get("/categories", categoryController.getCategories);
 router.post("/categories", globalErrors.validateBody(categoryCreateSchema), categoryController.createCategory);
 router.delete("/categories/:id", categoryController.deleteCategory);
 
