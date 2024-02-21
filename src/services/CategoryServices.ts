d66308acd7b7c2421145fc5d1b257b54e11a8a5f
JokerMSD <igor332004@gmail.com>
2024-02-21 17:42:11 -0300

    update CategoryServices.ts

--- src/services/CategoryServices.ts
+++ src/services/CategoryServices.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/database";
 import { CreateCategory } from "../interfaces/interfaces";
+import { category } from "../tests/mocks/category.mocks";
+
 
 export class CategoryController {
+
+  public getCategories = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const categories = await prisma.category.findMany();
+      return res.status(200).json(categories);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+  
+
   public createCategory = async ( req: Request, res: Response ): Promise<Response> => {
     try {
       const newTask: CreateCategory = req.body;
@@ -15,20 +29,24 @@ export class CategoryController {
   };
 
   public deleteCategory = async ( req: Request, res: Response ): Promise<Response> => {
-    try {
-      const categoryId = Number(req.params.id);
-      const deleted = await prisma.category.delete({
-        where: { id: categoryId },
-      });
+  try {
+    const categoryId = Number(req.params.id);
+    const category = await prisma.category.findUnique({
+      where: { id: categoryId },
+    });
 
-      if (!deleted) {
-        return res.status(404).json({ error: "Category not found." });
-      }
-
-      return res.status(204).send();
-    } catch (error) {
-      console.error("Error deleting category:", error);
-      return res.status(500).json({ error: "Internal server error" });
+    if (!category) {
+      return res.status(404).json({ error: "Category not found." });
     }
-  };
+
+    await prisma.category.delete({
+      where: { id: categoryId },
+    });
+
+    return res.status(204).send();
+  } catch (error) {
+    console.error("Error deleting category:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+ }
 }
