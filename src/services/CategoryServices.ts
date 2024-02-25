883c039d5a29659f16c04b0d5bd41dcb1e6d3b10
JokerMSD <igor332004@gmail.com>
2024-02-25 15:58:24 -0300

    update CategoryServices.ts

--- src/services/CategoryServices.ts
+++ src/services/CategoryServices.ts
@@ -1,52 +1,71 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/database";
 import { CreateCategory } from "../interfaces/interfaces";
-import { category } from "../tests/mocks/category.mocks";
 
+class AppError extends Error {
+  public statusCode: number;
 
-export class CategoryController {
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
 
-  public getCategories = async (req: Request, res: Response): Promise<Response> => {
+export class CategoryService {
+  public async getCategories(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
     try {
       const categories = await prisma.category.findMany();
       return res.status(200).json(categories);
     } catch (error) {
-      console.error("Error fetching tasks:", error);
-      return res.status(500).json({ error: "Internal server error" });
+      console.error("Error fetching categories:", error);
+      throw new AppError(500, "Internal server error" );
     }
-  };
-  
-
-  public createCategory = async ( req: Request, res: Response ): Promise<Response> => {
-    try {
-      const newTask: CreateCategory = req.body;
-      const createdTask = await prisma.category.create({ data: newTask });
-      return res.status(201).json(createdTask);
-    } catch (error) {
-      console.error("Error creating task:", error);
-      return res.status(500).json({ error: "Internal server error" });
-    }
-  };
-
-  public deleteCategory = async ( req: Request, res: Response ): Promise<Response> => {
-  try {
-    const categoryId = Number(req.params.id);
-    const category = await prisma.category.findUnique({
-      where: { id: categoryId },
-    });
-
-    if (!category) {
-      return res.status(404).json({ error: "Category not found." });
-    }
-
-    await prisma.category.delete({
-      where: { id: categoryId },
-    });
-
-    return res.status(204).send();
-  } catch (error) {
-    console.error("Error deleting category:", error);
-    return res.status(500).json({ error: "Internal server error" });
   }
- }
+
+  public async createCategory(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    try {
+      const newCategory: CreateCategory = req.body;
+      const createdCategory = await prisma.category.create({
+        data: newCategory,
+      });
+      return res.status(201).json(createdCategory);
+    } catch (error) {
+      console.error("Error creating category:", error);
+      throw new AppError(500, "Internal server error" );
+    }
+  }
+
+  public async deleteCategory(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    try {
+      const categoryId = Number(req.params.id);
+      const category = await prisma.category.findUnique({
+        where: { id: categoryId },
+      });
+
+      if (!category) {
+        throw new AppError(404, "Category not found.");
+      }
+
+      await prisma.category.delete({
+        where: { id: categoryId },
+      });
+
+      return res.status(204).send();
+    } catch (error) {
+      console.error("Error deleting category:", error);
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ error: error.message });
+      }
+      throw new AppError(500, "Internal server error" );
+    }
+  }
 }
