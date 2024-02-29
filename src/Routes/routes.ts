872551a69bc1aec1ffa4ca92a6486131d34bda07
JokerMSD <igor332004@gmail.com>
2024-02-29 15:56:49 -0300

    update routes.ts

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,6 +1,7 @@
+import { Router } from "express";
 import { TaskController } from "../controllers/TaskController";
 import { CategoryController } from "../controllers/CategoryControllers";
-import { Router } from "express";
+import { UserController } from "../controllers/UserController";
 import { CheckDuplicateTaskName, GlobalErrors } from "../middlewares/middleware";
 import { taskCreateSchema, taskUpdateSchema, categoryCreateSchema } from "../schemas/task.schemas";
 
@@ -8,7 +9,13 @@ const router = Router();
 const globalErrors = new GlobalErrors();
 const taskController = new TaskController();
 const categoryController = new CategoryController();
+const userController = new UserController();
 
+router.post("/users", userController.createUser);
+router.get("/users/profile", userController.getUsers);
+router.get("/users/:id", userController.getUserById);
+router.patch("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
 
 
 router.post("/tasks", CheckDuplicateTaskName.getInstance().execute, globalErrors.validateTitle, globalErrors.validateBody(taskCreateSchema), taskController.createTask);
@@ -16,6 +23,7 @@ router.get('/tasks', taskController.getTasks);
 router.get("/tasks/:id", taskController.getTaskById);
 router.patch("/tasks/:id",globalErrors.validateBody(taskUpdateSchema), taskController.updateTask);
 router.delete("/tasks/:id", taskController.deleteTask);
+
 
 router.get("/categories", categoryController.getCategories);
 router.post("/categories", globalErrors.validateBody(categoryCreateSchema), categoryController.createCategory);
