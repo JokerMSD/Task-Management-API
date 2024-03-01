b69844a3d5417c42ba1053438b9feda5289b663f
JokerMSD <igor332004@gmail.com>
2024-03-01 14:29:21 -0300

    feat: create src/schemas/Index.schemas.ts

---
+++ src/schemas/Index.schemas.ts
@@ -0,0 +1,14 @@
+import { categorySchema, categoryCreateSchema } from "./category.schema"
+import { userSchema, userCreateSchema } from "./user.schema"
+import { taskSchema, taskCreateSchema, taskUpdateSchema, taskArraySchema } from "./task.schema"
+
+export { 
+    categorySchema,
+    categoryCreateSchema,
+    userSchema,
+    userCreateSchema,
+    taskSchema,
+    taskCreateSchema,
+    taskUpdateSchema,
+    taskArraySchema
+ }
