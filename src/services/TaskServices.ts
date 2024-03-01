177e19b116ba07b9d86f7d16673c17cd291ef173
JokerMSD <igor332004@gmail.com>
2024-03-01 16:39:24 -0300

    update TaskServices.ts

--- src/services/TaskServices.ts
+++ src/services/TaskServices.ts
@@ -4,6 +4,35 @@ import { CreateTask } from "../interfaces/interfaces";
 import { AppError } from "../errors/AppError";
 
 export class TaskService {
+  public async createTask( req: Request, res: Response ): Promise<Response> {
+    try {
+      const newTask: CreateTask = req.body;
+
+      if (typeof req.body.title !== "string") {
+        return res.status(400).json({ error: "Title must be a string" });
+      }
+
+      const category = await prisma.category.findUnique({
+        where: { id: newTask.categoryId },
+      });
+      if (!category) {
+        return res.status(404).json({ message: "Category not found" });
+      }
+
+      const createdTask = await prisma.task.create({ data: newTask, include: { owner: true } });
+
+      return res.status(201).json(createdTask);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error creating task:", error);
+      } else {
+        console.error("An unexpected error occurred:", error);
+      }
+      return res
+        .status(500)
+        .json({ message: "An error occurred while creating the task" });
+    }
+  }
   public async getTasks(req: Request, res: Response): Promise<Response> {
     try {
       const categoryNameFilter = req.query.category
@@ -21,8 +50,9 @@ export class TaskService {
 
       const matchingTasks = await prisma.task.findMany({
         where: whereClause,
-        include: { category: true },
+        include: { category: true, owner: true },
       });
+
 
       if (matchingTasks.length === 0) {
         return res.status(404).json({ message: "No tasks found" });
@@ -30,6 +60,7 @@ export class TaskService {
 
       const response = matchingTasks.map((task) => {
         return {
+          owner: task.owner,
           id: task.id,
           title: task.title,
           content: task.content,
@@ -50,7 +81,7 @@ export class TaskService {
       const taskId = Number(req.params.id);
       const matchingTask = await prisma.task.findUnique({
         where: { id: taskId },
-        include: { category: true },
+        include: { category: true, owner: true },
       });
 
       if (!matchingTask) {
@@ -58,6 +89,7 @@ export class TaskService {
       }
 
       const response = {
+        owner: matchingTask.owner,
         id: matchingTask.id,
         title: matchingTask.title,
         content: matchingTask.content,
@@ -72,36 +104,6 @@ export class TaskService {
     }
   }
 
-  public async createTask(req: Request, res: Response): Promise<Response> {
-    try {
-      const newTask: CreateTask = req.body;
-
-      if (typeof req.body.title !== "string") {
-        return res.status(400).json({ error: "Title must be a string" });
-      }
-
-      const category = await prisma.category.findUnique({
-        where: { id: newTask.categoryId },
-      });
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
-      }
-
-      const createdTask = await prisma.task.create({ data: newTask });
-
-      return res.status(201).json(createdTask);
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error("Error creating task:", error);
-      } else {
-        console.error("An unexpected error occurred:", error);
-      }
-      return res
-        .status(500)
-        .json({ message: "An error occurred while creating the task" });
-    }
-  }
-
   public async updateTask(req: Request, res: Response): Promise<Response> {
     try {
       const taskId = Number(req.params.id);
@@ -111,7 +113,7 @@ export class TaskService {
         where: { id: taskId },
       });
       if (!existingTask) {
-        throw new AppError(404, "Task not found" );
+        throw new AppError(404, "Task not found");
       }
 
       if (updatedTaskData.categoryId) {
@@ -155,5 +157,4 @@ export class TaskService {
       return res.status(500).json({ error: "Internal server error" });
     }
   }
-  
 }
