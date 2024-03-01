ef50a97fe048481843d7f087ad559b3b087fc52c
JokerMSD <igor332004@gmail.com>
2024-03-01 16:39:17 -0300

    update user.schema.ts

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,27 +1,15 @@
 import { z } from "zod";
-import { categorySchema } from "./category.schema";
-import { taskSchema } from "./task.schema";
-
 
 const userSchema = z.object({
-    id: z.bigint().positive().transform((id) => id.toLocaleString()),
-    name: z.string().min(3).max(255),
-    email: z.string().email().max(255),
-    password: z.string().max(16),
-    task: taskSchema.nullish(),
-    category: categorySchema.nullish(),
-});
-  
-  const userCreateSchema = userSchema.pick({
-    name: true,
-    email: true,
-    password: true,
-    task: true,
-    category: true
+  id: z
+    .bigint()
+    .positive()
+    .transform((id) => id.toLocaleString()),
+  name: z.string().min(3).max(255),
+  email: z.string().email().max(255),
+  password: z.string().max(16)
 });
 
-export {
-    userSchema,
-    userCreateSchema,
-};
-  
+const userCreateSchema = userSchema.omit({ id: true });
+
+export { userSchema, userCreateSchema };
