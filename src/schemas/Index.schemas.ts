445ea665ef3649d450e1352f7ee2d14e55dc5f49
JokerMSD <igor332004@gmail.com>
2024-03-01 16:39:12 -0300

    update src/schemas/Index.schemas.ts

--- src/schemas/Index.schemas.ts
+++ src/schemas/Index.schemas.ts
@@ -1,14 +1,19 @@
-import { categorySchema, categoryCreateSchema } from "./category.schema"
-import { userSchema, userCreateSchema } from "./user.schema"
-import { taskSchema, taskCreateSchema, taskUpdateSchema, taskArraySchema } from "./task.schema"
+import { categorySchema, categoryCreateSchema } from "./category.schema";
+import { userSchema, userCreateSchema } from "./user.schema";
+import {
+  taskSchema,
+  taskCreateSchema,
+  taskUpdateSchema,
+  taskArraySchema,
+} from "./task.schema";
 
-export { 
-    categorySchema,
-    categoryCreateSchema,
-    userSchema,
-    userCreateSchema,
-    taskSchema,
-    taskCreateSchema,
-    taskUpdateSchema,
-    taskArraySchema
- }
+export {
+  categorySchema,
+  categoryCreateSchema,
+  userSchema,
+  userCreateSchema,
+  taskSchema,
+  taskCreateSchema,
+  taskUpdateSchema,
+  taskArraySchema,
+};
