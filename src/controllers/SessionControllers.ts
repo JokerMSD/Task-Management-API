e64a01ec85e6d78a845dd7d913d31a0aac077eac
JokerMSD <igor332004@gmail.com>
2024-03-05 15:30:10 -0300

    feat: create SessionControllers.ts

---
+++ src/controllers/SessionControllers.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { SessionService } from "../services/SessionServices";
+
+export class SessionController {
+  private sessionService = new SessionService();
+
+  public login = async (req: Request, res: Response) => {
+    const token = await this.sessionService.login(req.body);
+    return res.status(200).json(token);
+  };
+}
