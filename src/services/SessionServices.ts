4fc87a3e280df4ad39285de4eba3dea38f49d148
JokerMSD <igor332004@gmail.com>
2024-03-06 12:17:23 -0300

    update SessionServices.ts

--- src/services/SessionServices.ts
+++ src/services/SessionServices.ts
@@ -5,23 +5,28 @@ import { SessionCreate, SessionReturn } from "../interfaces/session.interface";
 import { sign } from "jsonwebtoken";
 
 export class SessionService {
-  public login = async({email, password}: SessionCreate): Promise<SessionReturn> => {
-    const foundUser = await prisma.user.findFirst({ where: {email} });
+  public login = async ({
+    email,
+    password,
+  }: SessionCreate): Promise<SessionReturn> => {
+    const foundUser = await prisma.user.findFirst({ where: { email } });
     if (!foundUser) {
-        throw new AppError(404, "User/email not found")
-    };
+      throw new AppError(401, "Invalid credentials");
+    }
 
     const pwdMatch = await compare(password, foundUser.password);
     if (!pwdMatch) {
-        throw new AppError(401, "Invalid credentials")
+      throw new AppError(401, "Invalid credentials");
     }
 
     const secret = process.env.SECRET_KEY!;
-    const expiresIn = process.env.EXPIRES_IN!
+    const expiresIn = process.env.EXPIRES_IN!;
 
-    const token = sign({id: foundUser.id}, secret, {expiresIn, subject: String(foundUser.id)})
-    
+    const accessToken = sign({ id: foundUser.id }, secret, {
+      expiresIn,
+      subject: String(foundUser.id),
+    });
 
-    return { token }
-  }
-};
+    return { accessToken };
+  };
+}
