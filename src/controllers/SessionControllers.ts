51703abd3bb628bfe9936a29b506bcb6fc017b2d
JokerMSD <igor332004@gmail.com>
2024-03-06 12:16:57 -0300

    update SessionControllers.ts

--- src/controllers/SessionControllers.ts
+++ src/controllers/SessionControllers.ts
@@ -5,7 +5,7 @@ export class SessionController {
   private sessionService = new SessionService();
 
   public login = async (req: Request, res: Response) => {
-    const token = await this.sessionService.login(req.body);
-    return res.status(200).json(token);
+    const accessToken = await this.sessionService.login(req.body);
+    return res.status(200).json(accessToken);
   };
 }
