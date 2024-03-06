26e4eb1432f5e4f649b6cc7f0bfe8dfbd799667f
JokerMSD <igor332004@gmail.com>
2024-03-06 12:38:18 -0300

    update session.interface.ts

--- src/interfaces/session.interface.ts
+++ src/interfaces/session.interface.ts
@@ -4,6 +4,11 @@ import { sessionCreateSchema } from "../schemas/session.schema";
 type SessionCreate = z.infer<typeof sessionCreateSchema>;
 type SessionReturn = {
   accessToken: string;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+  };
 };
 
 export { SessionCreate, SessionReturn };
