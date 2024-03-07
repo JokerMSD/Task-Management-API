5fb6efc8af79dcc0439591ecda75b3aa7070a45f
JokerMSD <igor332004@gmail.com>
2024-03-07 16:23:08 -0300

    feat: create src/middlewares/index.middleware.ts

---
+++ src/middlewares/index.middleware.ts
@@ -0,0 +1,11 @@
+import { CheckDuplicateTaskName, CheckTaskExistence } from "./task.middleware";
+import { GlobalErrors } from "./global.middleware";
+import { AuthMiddleware, PermissionMiddleware } from "./permission.middleware";
+
+export { 
+    GlobalErrors,
+    AuthMiddleware,
+    CheckTaskExistence,
+    PermissionMiddleware,
+    CheckDuplicateTaskName,
+ }
