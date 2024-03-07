54720808f4d19697bcf108b5eee7c3a01698eeb5
JokerMSD <igor332004@gmail.com>
2024-03-07 16:23:17 -0300

    update routes.ts

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,41 +1,42 @@
 import { Router } from "express";
-import { CategoryController } from "../controllers/CategoryControllers";
-import { categoryCreateSchema } from "../schemas/Index.schemas";
-import { SessionController } from "../controllers/SessionControllers";
-import { sessionCreateSchema } from "../schemas/session.schema";
-import { TaskController } from "../controllers/TaskController";
-import { taskCreateSchema, taskUpdateSchema } from "../schemas/Index.schemas";
-import { CheckDuplicateTaskName, GlobalErrors, AuthMiddleware } from "../middlewares/middleware";
 import { UserController } from "../controllers/UserController";
+import { TaskController } from "../controllers/TaskController";
 import { userCreateSchema } from "../schemas/Index.schemas";
+import { SessionController } from "../controllers/SessionControllers";
+import { CategoryController } from "../controllers/CategoryControllers";
+import { sessionCreateSchema } from "../schemas/session.schema";
+import { categoryCreateSchema } from "../schemas/Index.schemas";
+import { taskCreateSchema, taskUpdateSchema } from "../schemas/Index.schemas";
+import { CheckDuplicateTaskName, GlobalErrors, AuthMiddleware, PermissionMiddleware } from "../middlewares/index.middleware";
 
 
 
 const appRouter = Router();
+const auth = new AuthMiddleware();
+const admin = new PermissionMiddleware()
 const globalErrors = new GlobalErrors();
-const categoryController = new CategoryController();
 const userController = new UserController();
 const taskController = new TaskController();
 const sessionController = new SessionController();
-const auth = new AuthMiddleware();
+const categoryController = new CategoryController();
 
 
 
 appRouter.post("/users", globalErrors.validateBody(userCreateSchema), userController.createUser);
 appRouter.get("/users/profile", auth.isAuthenticated, userController.getUsers);
-appRouter.delete("/users/:id", userController.deleteUser);
+appRouter.delete("/users/:id", auth.isAuthenticated, admin.isAdminOrOwnerUser, userController.deleteUser);
 
 
-appRouter.post("/tasks", CheckDuplicateTaskName.getInstance().execute, globalErrors.validateTitle, globalErrors.validateBody(taskCreateSchema), taskController.createTask);
-appRouter.get("/tasks", taskController.getTasks);
-appRouter.get("/tasks/:id", taskController.getTaskById);
-appRouter.patch( "/tasks/:id", globalErrors.validateBody(taskUpdateSchema), taskController.updateTask);
-appRouter.delete("/tasks/:id", taskController.deleteTask);
+appRouter.post("/tasks", auth.isAuthenticated, CheckDuplicateTaskName.getInstance().execute, globalErrors.validateTitle, globalErrors.validateBody(taskCreateSchema), taskController.createTask);
+appRouter.get("/tasks", auth.isAuthenticated, taskController.getTasks);
+appRouter.get("/tasks/:id", auth.isAuthenticated, taskController.getTaskById);
+appRouter.patch( "/tasks/:id", auth.isAuthenticated, globalErrors.validateBody(taskUpdateSchema), taskController.updateTask);
+appRouter.delete("/tasks/:id", auth.isAuthenticated, taskController.deleteTask);
 
 
-appRouter.get("/categories", categoryController.getCategories);
-appRouter.post( "/categories", globalErrors.validateBody(categoryCreateSchema), categoryController.createCategory);
-appRouter.delete("/categories/:id", categoryController.deleteCategory);
+appRouter.get("/categories", auth.isAuthenticated, categoryController.getCategories);
+appRouter.post( "/categories", auth.isAuthenticated, globalErrors.validateBody(categoryCreateSchema), categoryController.createCategory);
+appRouter.delete("/categories/:id", auth.isAuthenticated, categoryController.deleteCategory);
 
 
 appRouter.post("/users/login", globalErrors.validateBody(sessionCreateSchema), sessionController.login);
