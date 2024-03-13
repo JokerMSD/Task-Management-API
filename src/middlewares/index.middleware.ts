dafcb2ade99e6a9b10e940f84ccb1b9051f4d172
JokerMSD <igor332004@gmail.com>
2024-03-13 13:58:51 -0300

    update src/middlewares/index.middleware.ts

--- src/middlewares/index.middleware.ts
+++ src/middlewares/index.middleware.ts
@@ -2,10 +2,10 @@ import { CheckDuplicateTaskName, CheckTaskExistence } from "./task.middleware";
 import { GlobalErrors } from "./global.middleware";
 import { AuthMiddleware, PermissionMiddleware } from "./permission.middleware";
 
-export { 
-    GlobalErrors,
-    AuthMiddleware,
-    CheckTaskExistence,
-    PermissionMiddleware,
-    CheckDuplicateTaskName,
- }
+export {
+  GlobalErrors,
+  AuthMiddleware,
+  CheckTaskExistence,
+  PermissionMiddleware,
+  CheckDuplicateTaskName,
+};
