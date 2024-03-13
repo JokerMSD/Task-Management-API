23c5b4624b3336f93cae1ca615ef2207ba670ffe
JokerMSD <igor332004@gmail.com>
2024-03-13 13:59:03 -0300

    update task.schema.ts

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -2,28 +2,22 @@ import { z } from "zod";
 import { userSchema } from "./user.schema";
 import { categorySchema } from "./category.schema";
 
-
 const taskSchema = z.object({
   id: z.bigint().positive().transform((id) => id.toLocaleString()),
   title: z.string().min(3),
   content: z.string().min(3),
   finished: z.boolean().optional().default(false),
-  categoryId: z.number(),
+  categoryId: z.number().optional().nullable(),
   owner: userSchema,
   category: categorySchema,
 });
 
-const taskCreateSchema = taskSchema.omit({id:true, owner: true, category: true}).extend({ownerId: z.number().positive()});
+const taskCreateSchema = taskSchema
+  .omit({ id: true, owner: true, category: true })
+  .extend({ userId: z.number().positive().nullish() });
 
-const taskUpdateSchema = taskCreateSchema.omit({ownerId: true}).partial();
+const taskUpdateSchema = taskCreateSchema.omit({ userId: true }).partial();
 
 const taskArraySchema = taskSchema.array();
 
-export {
-  taskSchema,
-  taskCreateSchema,
-  taskUpdateSchema,
-  taskArraySchema,
-};
-
-
+export { taskSchema, taskCreateSchema, taskUpdateSchema, taskArraySchema };
